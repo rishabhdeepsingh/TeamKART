a7009c45e0e40742679bea5f4dbbec8e43d391b7
Rishabh Deep Singh <rishabhdeepsingh98@gmail.com>
2018-12-11 23:57:41 +0530

    2nd Commit

--- app/src/main/java/com/rishabhdeepsingh/teamkart/MainActivity.kt
+++ app/src/main/java/com/rishabhdeepsingh/teamkart/MainActivity.kt
@@ -11,72 +11,80 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
-import kotlinx.android.synthetic.main.daq_activity.view.*
 import kotlinx.android.synthetic.main.main_activity.*
 
 
 class MainActivity : AppCompatActivity() {
+    fun forumActivity(item: MenuItem) {
+        System.err.println("FORUM Layout selected From MainActivity")
+        setContentView(R.layout.main_activity)
+    }
+
+    fun daqActivity(item: MenuItem) {
+        System.err.println("DAQ Layout selected From MainActivity")
+        setContentView(R.layout.daq_activity)
+        val buttonSelectAll = findViewById<Button>(R.id.btn_selectAll)
+        val buttonGetDetails = findViewById<Button>(R.id.btn_get_details)
+        val daqList = findViewById<LinearLayout>(R.id.list_daq)
+        val daqListButtons = findViewById<LinearLayout>(R.id.daq_buttons)
+        val topButtonsDAQ = findViewById<RelativeLayout>(R.id.topButtons)
+        val viewRPM = findViewById<LinearLayout>(R.id.view_rpm_image)
+
+        val buttonRPM = findViewById<Button>(R.id.btn_rpm)
+        buttonSelectAll.setOnClickListener {
+            val checkBox = ArrayList<CheckBox>()
+            checkBox.add(findViewById(R.id.checkBox_rpmOfWheels) as CheckBox)
+            checkBox.add(findViewById(R.id.checkBox_acceleration) as CheckBox)
+            checkBox.add(findViewById(R.id.checkBox_steeringAngle) as CheckBox)
+            checkBox.add(findViewById(R.id.checkBox_coolentTemperature) as CheckBox)
+            checkBox.add(findViewById(R.id.checkBox_crankShaftRPM) as CheckBox)
+            checkBox.add(findViewById(R.id.checkBox_throttlePosition) as CheckBox)
+            checkBox.add(findViewById(R.id.checkBox_coolentFlow) as CheckBox)
+            checkBox.add(findViewById(R.id.checkBox_breakPressure) as CheckBox)
+            checkBox.add(findViewById(R.id.checkBox_fuelLevel) as CheckBox)
+            checkBox.add(findViewById(R.id.checkBox_intakeAirPressure) as CheckBox)
+            for (i in checkBox) {
+                i.isChecked = true
+            }
+        }
+        buttonGetDetails.setOnClickListener {
+            daqList.visibility = View.GONE
+            daqListButtons.visibility = View.VISIBLE
+            topButtonsDAQ.visibility = View.GONE
+        }
+        buttonRPM.setOnClickListener {
+            println("[+] RPM of Wheel")
+            daqListButtons.visibility = View.GONE
+            viewRPM.visibility = View.VISIBLE
+        }
+    }
+
+    fun servicesActivity(item: MenuItem) {
+        setContentView(R.layout.services_activity)
+        System.err.println("Services Layout selected From MainActivity")
+        val goButton = findViewById<Button>(R.id.btn_go)
+        val findServiceButton = findViewById<Button>(R.id.btn_find_service)
+        val serviceLayout = findViewById<LinearLayout>(R.id.services_layout)
+        val findService = findViewById<LinearLayout>(R.id.find_service)
+        findServiceButton.setOnClickListener {
+            serviceLayout.visibility = View.GONE
+            findService.visibility = View.VISIBLE
+        }
+        val layoutData = findViewById<LinearLayout>(R.id.services_information)
+        goButton.setOnClickListener {
+            layoutData.visibility = View.VISIBLE
+        }
+    }
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_daq -> {
-                System.err.println("DAQ Layout selected From MainActivity")
-                setContentView(R.layout.daq_activity)
-                val buttonSelectAll = findViewById<Button>(R.id.btn_selectAll)
-                val buttonGetDetails = findViewById<Button>(R.id.btn_get_details)
-                val daqList = findViewById<LinearLayout>(R.id.list_daq)
-                val daqListButtons = findViewById<LinearLayout>(R.id.daq_buttons)
-                val topButtonsDAQ = findViewById<RelativeLayout>(R.id.topButtons)
-                val viewRPM = findViewById<LinearLayout>(R.id.view_rpm_image)
-
-                val buttonRPM = findViewById<Button>(R.id.btn_rpm)
-                buttonSelectAll.setOnClickListener {
-                    val checkBox = ArrayList<CheckBox>()
-                    checkBox.add(findViewById(R.id.checkBox_rpmOfWheels) as CheckBox)
-                    checkBox.add(findViewById(R.id.checkBox_acceleration) as CheckBox)
-                    checkBox.add(findViewById(R.id.checkBox_steeringAngle) as CheckBox)
-                    checkBox.add(findViewById(R.id.checkBox_coolentTemperature) as CheckBox)
-                    checkBox.add(findViewById(R.id.checkBox_crankShaftRPM) as CheckBox)
-                    checkBox.add(findViewById(R.id.checkBox_throttlePosition) as CheckBox)
-                    checkBox.add(findViewById(R.id.checkBox_coolentFlow) as CheckBox)
-                    checkBox.add(findViewById(R.id.checkBox_breakPressure) as CheckBox)
-                    checkBox.add(findViewById(R.id.checkBox_fuelLevel) as CheckBox)
-                    checkBox.add(findViewById(R.id.checkBox_intakeAirPressure) as CheckBox)
-                    for (i in checkBox) {
-                        i.isChecked = true
-                    }
-                }
-                buttonGetDetails.setOnClickListener {
-                    daqList.visibility = View.GONE
-                    daqListButtons.visibility = View.VISIBLE
-                    topButtonsDAQ.visibility = View.GONE
-                }
-                buttonRPM.setOnClickListener {
-                    println("[+] RPM of Wheel")
-                    daqListButtons.visibility = View.GONE
-                    viewRPM.visibility = View.VISIBLE
-                }
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_forum -> {
-                setContentView(R.layout.main_activity)
-                System.err.println("FORUM Layout selected From MainActivity")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_services -> {
-                setContentView(R.layout.services_activity)
-                System.err.println("Services Layout selected From MainActivity")
-                val goButton = findViewById<Button>(R.id.btn_go)
-                val findServiceButton = findViewById<Button>(R.id.btn_find_service)
-                val serviceLayout = findViewById<LinearLayout>(R.id.services_layout)
-                val findService = findViewById<LinearLayout>(R.id.find_service)
-                findServiceButton.setOnClickListener {
-                    serviceLayout.visibility = View.GONE
-                    findService.visibility = View.VISIBLE
-                }
-                val layoutData = findViewById<LinearLayout>(R.id.services_information)
-                goButton.setOnClickListener {
-                    layoutData.visibility = View.VISIBLE
-                }
                 return@OnNavigationItemSelectedListener true
             }
         }
